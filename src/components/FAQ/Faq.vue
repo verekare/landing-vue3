97e5843c673127b364077c7d3b095aea12590c0b
Anastasia Shadrina <snasfibz05@gmail.com>
2024-08-14 16:31:13 +0200

    Accoridion rebuilt

--- src/components/FAQ/Faq.vue
+++ src/components/FAQ/Faq.vue
@@ -1,27 +1,64 @@
 <template>
-  <div class="rowLayout">
-    <div :class="$style.faqLayout">
-      <h2 :class="$style.contactTitle">{{ content.faq.title }}</h2>
-      <div :class="$style.contactDesctiption">
-        <span>{{ content.faq.description }}</span>
-        <span :class="$style.emphasizedText">{{ content.faq.emphasized }}</span>
-      </div>
-    </div>
-    <div :class="$style.accordionBlock">
-      <div v-for="question in FAQ_CONTENT">
-        <details>
-          <summary :class="$style.faqQuestion">{{ question.question }}</summary>
-          <p :class="$style.faqAnswer">{{ question.answer }}<</p>
-        </details>
-        <span :class="$style.divider"></span>
+  <div class="container">
+    <div class="rowLayout">
+      <SectionHead :content="content.faq" :class="$style.sectionHead" />
+
+      <div :class="$style.faqsAccordion">
+        <div v-for="(faq, index) in FAQS" key="index">
+          <div :class="$style.faqElement">
+            <div :class="$style.faqHeader" @click="() => handleClick(index)">
+              <div :class="$style.faqMarker">
+                <Icon
+                  :iconPath="openedStates[index] ? ICONS_PATHS.minus : ICONS_PATHS.plus"
+                  :class="[openedStates[index] && style.activeIcon]"
+                />
+              </div>
+              <h3
+                :class="[style.faqQuestion, openedStates[index] && style.openedQuestion]"
+              >
+                {{ faq.question }}
+              </h3>
+            </div>
+            <div :class="[style.faqAnswer, openedStates[index] && style.openedAnswer]">
+              <p>{{ faq.answer }}</p>
+            </div>
+          </div>
+          <span v-if="index != FAQS.length" :class="$style.divider"></span>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { useCssModule, ref, computed, normalizeClass } from 'vue';
 import content from '@/constants/sectionHeaders';
-import { FAQ_CONTENT } from '@/constants/services';
+import { FAQS } from '@/constants/services';
+import SectionHead from '@/components/SectionHead/SectionHead.vue';
+import Icon from '@/ui-kit/Icon/Icon.vue';
+import { ICONS_PATHS } from '@/constants/icons';
+
+let openedStates = ref(FAQS.map(() => false));
+const handleClick = (index) => {
+  openedStates.value[index] = !openedStates.value[index];
+  console.log(openedStates);
+};
+
+const style = useCssModule();
+
+const faqQuestionClassName = (index) => {
+  return normalizeClass([
+    style.faqQuestion,
+    openedStates.value[index] && style.openedQuestion,
+  ]);
+};
+
+// const faqAnswerClassName = (index) => {
+//   return normalizeClass([
+//     style.faqAnswer,
+//     openedStates.value[index] && style.openedAnswer,
+//   ]);
+// };
 </script>
 <style module lang="scss">
 @import './Faq.module.scss';
