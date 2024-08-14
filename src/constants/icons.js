7ae54ca82c75ebd5948448b0125dafff53c2620c
Anastasia Shadrina <snasfibz05@gmail.com>
2024-08-14 16:34:12 +0200

    Icons added

---
+++ src/constants/icons.js
@@ -0,0 +1,5 @@
+export const ICONS_PATHS = {
+  plus: 'src/assets/icon_plus.svg',
+  minus: 'src/assets/icon_minus.svg',
+  arrow: 'assets/icon_arrow.svg',
+};
---
+++ src/ui-kit/Icon/Icon.vue
@@ -0,0 +1,20 @@
+<template>
+  <span :class="$style.iconWrapper">
+    <InlineSvg :src="iconPath"></InlineSvg>
+  </span>
+</template>
+
+<script setup>
+import InlineSvg from 'vue-inline-svg';
+
+defineProps({
+  iconPath: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style module lang="scss">
+@import './Icon.module.scss';
+</style>
