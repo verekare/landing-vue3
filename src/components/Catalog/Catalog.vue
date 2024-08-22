938226120a4833ae231055f1f867d1bf94f8725d
Anastasia Shadrina <snasfibz05@gmail.com>
2024-08-22 18:22:37 +0200

    test slider

--- src/components/Catalog/Catalog.vue
+++ src/components/Catalog/Catalog.vue
@@ -2,20 +2,20 @@
   <div class="container">
     <div class="section">
       <SectionHead :content="content.catalog" />
-      <Swiper 
+      <swiper-container 
         :slides-per-view="SERVICES_ROOMS.list.length" :space-between="8"
-        @swiper="onSwiper" @slideChange="onSlideChange"
+        @swiper="onSwiper" @swiperslidechange="onSlideChange" ref="swiper"
       >
         <SwiperSlide 
           v-for="(roomCard, index) in SERVICES_ROOMS.list" :key="index" 
           :class="$style.gallery__item"
-          :style="{ width: index === activeIndex ? '50vw' : '8vw' }"
         >
           <ImageCard
             :class="[$style.gallery__item, isActive && $style.active ]"
+            :style="{ width: index === activeIndex ? '50vw' : '8vw' }"
             :image-path="roomCard.path"
           >
-            <div :class="$style.gallery__item__block">
+            <div :class="$style.gallery__item__block" v-show="index === activeIndex">
               <div :class="$style.gallery__item__block__info">
                 <h3 :class="$style.gallery__item__block__info__title">{{ roomCard.name }}</h3>
                 <p :class="$style.gallery__item__block__info__details">{{ roomCard.details }}</p>
@@ -24,7 +24,7 @@
             </div>
         </ImageCard>
         </SwiperSlide>
-      </Swiper>
+      </swiper-container>
     </div>
   </div>
 </template>
@@ -35,18 +35,23 @@ import content from '@/constants/sectionHeaders';
 import { SERVICES_ROOMS } from '@/constants/services';
 import ImageCard from '@/ui-kit/ImageCard/ImageCard.vue';
 import SecondaryButton from '@/ui-kit/SecondaryButton/SecondaryButton.vue';
-import { ref, onMounted, computed } from 'vue';
-import { Swiper, SwiperSlide } from 'swiper/vue';
+import { ref, computed } from 'vue';
+import { register } from 'swiper/element/bundle';
 import 'swiper/css';
-// import { Autoplay } from 'swiper/modules';
+import Swiper from 'swiper';
 
-// const modules = [Autoplay];
+register();
+
 const activeIndex = ref(0);
+// const swiper = new Swiper(swiper)
+
+// const isActive = computed((swiper) => activeIndex.value === swiper.activeIndex)
 
 const onSwiper = (swiper) => {
   console.log(swiper);
 };
-const onSlideChange = (swiper) => {
+const onSlideChange = (e) => {
+  console.log(e);
   console.log('slide change');
   console.log(swiper);
   activeIndex.value = swiper.activeIndex
---
+++ src/components/TestSlider/TestSlider.vue
@@ -0,0 +1,81 @@
+<template>
+<div class="container">
+  <swiper 
+    :loop="true" 
+    :slides-per-view="4" 
+    :space-between="8" 
+    slideActiveClass="swiper-slide-active"
+    centered-slides="true" 
+    @swiper="onSwiper"
+    class="swiper"
+  >
+    <div class="swiper-wrapper">
+      <swiper-slide> <div class="box one">img 1</div> </swiper-slide>
+      <swiper-slide> <div class="box two">vid 1</div> </swiper-slide>
+      <swiper-slide> <div class="box three">img 2</div> </swiper-slide>
+      <swiper-slide> <div class="box four">img 3</div> </swiper-slide>
+    </div>
+  </swiper>
+</div>
+</template>
+
+<script setup>
+import { register } from 'swiper/element/bundle';
+import 'swiper/css';
+register();
+
+const onSwiper = (swiper) => {
+  window.swiper = swiper;
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  padding: 7.5em 2.5em;
+  max-width: 1200px;
+  display: flex;
+  flex-direction: row;
+}
+
+.swiper {
+  position: relative;
+}
+
+.swiper-wrapper {
+  display: flex;
+  flex-direction: row;
+}
+
+.swiper-slide {
+  flex-shrink: 0;
+  flex-basis: 0;
+  width: 10vw;
+  transition: flex-basis 0.8s cubic-bezier(0.22, 1, 0.36, 1), width 0.8s cubic-bezier(0.22, 1, 0.36, 1);
+}
+
+.swiper-slide.swiper-slide-active{
+  flex-basis: 50%;
+  width: auto;
+}
+
+.box {
+  position: relative;
+  height: 40vh;
+}
+
+.one {
+  background-color: tomato;
+}
+
+.two {
+  background-color: turquoise;
+}
+
+.three {
+  background-color: palevioletred;
+}
+
+.four {
+  background-color: royalblue;
+}
+</style>
