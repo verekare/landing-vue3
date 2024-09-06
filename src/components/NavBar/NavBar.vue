848d8dc9721e781e712e8edb2799c247cee6435c
Anastasia Shadrina <snasfibz05@gmail.com>
2024-09-06 16:40:15 +0200

    add responsive menu

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -1,25 +1,79 @@
 <template>
-  <div :class="$style['nav-bar']">
-    <div :class="$style['nav-bar__brand-block']">
-      <img :class="$style['nav-bar__brand-block__logo']" alt="Company logo" src="@/assets/logo.svg" />
-      <span :class="$style['nav-bar__brand-block__title']">Golden<br />Wood</span>
+  <div :class="[$style['nav-bar'], isMenuOpen  && $style['nav-bar--mobile']]">
+  
+    <div :class="$style['nav-bar--touch']">
+      <div 
+        :class="[$style['nav-bar__brand-block'], isMenuOpen && $style['nav-bar__brand-block--mobile']]">
+        <InlineSvg :class="[$style['nav-bar__brand-block__logo'], isMenuOpen && $style['nav-bar__brand-block__logo--active']]" alt="Company logo" src="/images/logo.svg"></InlineSvg>
+        <span :class="[$style['nav-bar__brand-block__title'], isMenuOpen && $style['nav-bar__brand-block__title--mobile']]">Golden<br />Wood</span>
+      </div>
+      <input v-if="isMobile" type="checkbox" name="burger" id="burger" :class="$style['nav-bar__checkbox']">
+      <label v-if="isMobile" for="burger" :class="$style['nav-bar__burger']" @click="toggleMenu">
+        <div :class="$style['nav-bar__burger__icon']">
+          <div :class="[$style['nav-bar__burger__icon__line'], isMenuOpen && $style['nav-bar__burger__icon__line--active']]"></div>
+          <div :class="[$style['nav-bar__burger__icon__line'], isMenuOpen && $style['nav-bar__burger__icon__line--active']]"></div>
+        </div>
+      </label>
     </div>
-    <div :class="$style['nav-bar__menu']">
-    <nav :class="$style['nav-bar__menu__sections-list']">
-      <button :class="$style['nav-bar__menu__sections-list__title']" v-for="section in NAV_SECTIONS">
-        <span>{{ section.name }}</span>
-      </button>
-    </nav>
-    <PrimaryButton :class="$style['nav-bar__menu__contact']">
-      <span>Связаться</span>
-    </PrimaryButton>
+
+      <div :class="[$style['nav-bar__menu'], isMenuOpen && $style['nav-bar__menu--mobile']]">
+      <nav :class="[$style['nav-bar__menu__sections-list'], isMenuOpen && $style['nav-bar__menu__sections-list--mobile']]">
+        <TransitionGroup appear @before-enter="onBeforeEnter" @enter="onEnter">
+          <button
+            v-if="isMenuOpen || !isMobile"
+            :class="[$style['nav-bar__menu__sections-list__title'], (isMenuOpen) && $style['nav-bar__menu__sections-list__title--mobile']]"
+            v-for="(section, index) in NAV_SECTIONS"
+            :data-index="index">
+            <span>{{ section.name }}</span>
+          </button>
+        </TransitionGroup>
+      </nav>
+      <PrimaryButton :class="[$style['nav-bar__menu__contact'], isMenuOpen && $style['nav-bar__menu__contact--mobile']]">
+        <span>Связаться</span>
+      </PrimaryButton>
     </div>
+
   </div>
 </template>
 
 <script setup>
 import { NAV_SECTIONS } from '@/constants/services';
 import PrimaryButton from '@/ui-kit/PrimaryButton/PrimaryButton.vue';
+import InlineSvg from 'vue-inline-svg';
+import { onMounted, ref } from 'vue';
+import gsap from 'gsap';
+
+const isMobile = ref(window.innerWidth < 1025);
+
+onMounted(() => {
+  window.addEventListener('resize', () => {
+    isMobile.value = window.innerWidth < 1025;
+    console.log(isMobile.value);
+  })
+});
+
+const isMenuOpen = ref(false);
+
+const toggleMenu = () => isMobile && (isMenuOpen.value = !isMenuOpen.value);
+
+const onBeforeEnter = (element) => {
+  if (isMobile.value) {
+    element.style.opacity = 0;
+    element.style.transform = 'translateX(200px)'
+  }
+}
+
+const onEnter = (element, done) => {
+  if (isMobile.value) {
+    gsap.to(element, {
+      opacity: 1,
+      x: 0,
+      duration: 0.3,
+      onComplete: done,
+      delay: element.dataset.index * 0.12,
+    })
+  }
+}
 </script>
 
 <style module lang="scss">
